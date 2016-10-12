d3d29b483310320dc814c6b0cc9bc087c0905759
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-11 22:06:37 -0400

    Fix tests

--- tests/test_plocate.py
+++ tests/test_plocate.py
@@ -33,8 +33,6 @@ class Testplocate(unittest.TestCase):
     def test_command_line_interface(self):
         runner = CliRunner()
         result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'plocate.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert '-h, --help                 Show this message and exit.' in help_result.output
